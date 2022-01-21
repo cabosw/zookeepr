a7cfeb9dd53936923eb0c13ec80445535b9565c6
Abigail Oswald <abigailoswald@Abigails-MacBook-Pro.local>
2022-01-20 16:52:54 -0800

    user data creation/validation

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
 // import data
 const { animals } = require('./data/animals');
 
 // import express.js npm package
 const express = require('express');
 
+
 // port is defined by environment (ie heroku 80)
 const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -53,6 +62,32 @@ function findById(id, animalsArray) {
   return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
 // routes for front end to request data from
 app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -74,9 +109,16 @@ app.get('/api/animals/:id', (req, res) => {
 
 // POST requests are client requesting server to accept data
 app.post('/api/animals', (req, res) => {
-	// req.body is where our incoming content will be
-	console.log(req.body);
-	res.json(req.body);
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 //Bind and listen to connections on the specified host (website address) and port (website address but more specific?)      
